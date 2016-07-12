58ccf722e59591b8fb672d96d33165eb53503f0f
Asmod4n <Asmod4n@users.noreply.github.com>
2016-07-12 14:45:53 +0200

    add tls_handshake

--- src/mrb_tls.c
+++ src/mrb_tls.c
@@ -1,7 +1,7 @@
 ﻿#include "mruby/tls.h"
 #include "mrb_tls.h"
 
-static inline int
+MRB_INLINE int
 mrb_conf_tls(mrb_state* mrb, tls_t* ctx, mrb_value config_obj)
 {
     mrb_data_check_type(mrb, config_obj, &tls_config_type);
@@ -184,7 +184,7 @@ mrb_tls_config_set_protocols(mrb_state* mrb, mrb_value self)
 
     mrb_get_args(mrb, "i", &protocols);
 
-    if (protocols < 0 || protocols > UINT32_MAX) {
+    if (protocols < 0||protocols > UINT32_MAX) {
         mrb_raise(mrb, E_RANGE_ERROR, "protocols doesn't fit into UINT32");
     }
 
@@ -200,7 +200,7 @@ mrb_tls_config_set_verify_depth(mrb_state* mrb, mrb_value self)
 
     mrb_get_args(mrb, "i", &verify_depth);
 
-    if (verify_depth < INT_MIN || verify_depth > INT_MAX) {
+    if (verify_depth < INT_MIN||verify_depth > INT_MAX) {
         mrb_raise(mrb, E_RANGE_ERROR, "verify_depth doesn't fit into INT");
     }
 
@@ -320,7 +320,7 @@ mrb_tls_accept_socket(mrb_state* mrb, mrb_value self)
 
     mrb_get_args(mrb, "i", &socket);
 
-    if (socket < INT_MIN || socket > INT_MAX) {
+    if (socket < INT_MIN||socket > INT_MAX) {
         mrb_raise(mrb, E_RANGE_ERROR, "socket doesn't fit into INT");
     }
 
@@ -379,11 +379,11 @@ mrb_tls_connect_fds(mrb_state* mrb, mrb_value self)
 
     mrb_get_args(mrb, "iiz", &fd_read, &fd_write, &hostname);
 
-    if (fd_read < INT_MIN || fd_read > INT_MAX) {
+    if (fd_read < INT_MIN||fd_read > INT_MAX) {
         mrb_raise(mrb, E_RANGE_ERROR, "fd_read doesn't fit into INT");
     }
 
-    if (fd_write < INT_MIN || fd_write > INT_MAX) {
+    if (fd_write < INT_MIN||fd_write > INT_MAX) {
         mrb_raise(mrb, E_RANGE_ERROR, "fd_write doesn't fit into INT");
     }
 
@@ -412,7 +412,7 @@ mrb_tls_connect_socket(mrb_state* mrb, mrb_value self)
 
     mrb_get_args(mrb, "iz", &socket, &hostname);
 
-    if (socket < INT_MIN || socket > INT_MAX)
+    if (socket < INT_MIN||socket > INT_MAX)
         mrb_raise(mrb, E_RANGE_ERROR, "socket doesn't fit into INT");
 
     errno = 0;
@@ -502,7 +502,28 @@ mrb_tls_close(mrb_state* mrb, mrb_value self)
     return self;
 }
 
-void mrb_mruby_tls_gem_init(mrb_state* mrb)
+static mrb_value
+mrb_tls_handshake(mrb_state *mrb, mrb_value self)
+{
+    errno = 0;
+    int rc = tls_handshake((tls_t*)DATA_PTR(self));
+    if (rc == 0)
+        return self;
+    else if (rc == TLS_WANT_POLLIN) {
+        mrb_raise(mrb, E_TLS_WANT_POLLIN, tls_error((tls_t*)DATA_PTR(self)));
+    }
+    else if (rc == TLS_WANT_POLLOUT) {
+        mrb_raise(mrb, E_TLS_WANT_POLLOUT, tls_error((tls_t*)DATA_PTR(self)));
+    } else {
+        mrb_assert(rc == -1);
+        mrb_sys_fail(mrb, tls_error((tls_t*)DATA_PTR(self)));
+    }
+
+    return self;
+}
+
+void
+mrb_mruby_tls_gem_init(mrb_state* mrb)
 {
     struct RClass *tls_mod, *tls_proto_mod, *tls_conf_c, *tls_ctx_c, *tls_cli_c, *tls_server_c;
 
@@ -540,6 +561,7 @@ void mrb_mruby_tls_gem_init(mrb_state* mrb)
     mrb_define_method(mrb, tls_ctx_c, "read", mrb_tls_read, MRB_ARGS_OPT(2));
     mrb_define_method(mrb, tls_ctx_c, "write", mrb_tls_write, MRB_ARGS_ARG(1, 1));
     mrb_define_method(mrb, tls_ctx_c, "close", mrb_tls_close, MRB_ARGS_NONE());
+    mrb_define_method(mrb, tls_ctx_c, "handshake", mrb_tls_handshake, MRB_ARGS_NONE());
 
     tls_cli_c = mrb_define_class_under(mrb, tls_mod, "Client", tls_ctx_c);
     mrb_define_method(mrb, tls_cli_c, "initialize", mrb_tls_client, MRB_ARGS_OPT(1));
@@ -557,6 +579,4 @@ void mrb_mruby_tls_gem_init(mrb_state* mrb)
     }
 }
 
-void mrb_mruby_tls_gem_final(mrb_state* mrb)
-{
-}
+void mrb_mruby_tls_gem_final(mrb_state* mrb) {}
